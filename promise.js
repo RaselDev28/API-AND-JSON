b16e934f9f0494bc13cf4ce765786e0bc0201aa7
RaselDev28 <rasel.dev28@gmail.com>
2026-01-12 21:51:31 +0600

    more js

---
+++ promise.js
@@ -0,0 +1,12 @@
+const urls = [
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/photos"
+];
+
+Promise.all(urls.map(url => {
+    return fetch(url).then(res => res.json())
+    .then(data => data)
+}))
+    .then(res => res)
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
